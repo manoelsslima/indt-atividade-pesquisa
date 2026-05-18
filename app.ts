97d1ba52885ae8f76068870dfa3893d639eb3154
Manoel Lima <manoel.lima@manaus.am.gov.br>
2026-05-18 18:38:45 -0400

    adicionando generics

--- app.ts
+++ app.ts
@@ -2,9 +2,21 @@ import Pessoa from './Pessoa';
 import Gerente from './Gerente';
 
 // questao 2 - objeto eh o objeto concreto, criado a partir da classe
-const pessoa1 = new Pessoa("Manoel", 99);
+const pessoa1 = new Pessoa("Pessoa 1", 99);
 pessoa1.seApresentar();
 
 const gerente = new Gerente("Gerente 1", 30, 3000.0);
 gerente.seApresentar();
 gerente.autenticar("admin@email.com", "123");
+
+
+function listar<T extends Pessoa>(lista: T[]): void {
+    console.log("====================================");
+    console.log("============= LISTANDO =============");
+    console.log("====================================");
+    lista.forEach(p => {
+        console.log(p.getNome());
+    });
+}
+
+listar([pessoa1, gerente]);
