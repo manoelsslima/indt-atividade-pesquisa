71b01e66a61f196e0933c5ed9a232ce5cd8e6e67
Manoel Lima <manoel.lima@manaus.am.gov.br>
2026-05-18 17:53:12 -0400

    Pessoa com respostas 1, 2 e 3

---
+++ Pessoa.ts
@@ -0,0 +1,27 @@
+class Pessoa { // questao 1 - classe eh o modelo de objeto
+
+    private nome: string; // questao 2- atributos
+    private idade: number; // questao 3 - atributo private
+
+    constructor(nome: string, idade: number) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+     // questao 3 - encapsulamento do atributo 'nome'
+    public getNome(): string {
+        return this.nome;
+    }
+
+    // questao 3 - metodo getIdade eh public
+    public getIdade(): number {
+        return this.idade;
+    }
+
+    public seApresentar(): string {
+        return `Olá! Meu nome é ${this.nome}. Tenho ${this.idade} anos.`;
+    }
+}
+
+// questao 2 - objeto eh o objeto concreto, criado a partir da classe
+const manoel = new Pessoa("Manoel", 99);
